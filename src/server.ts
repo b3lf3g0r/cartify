7d19292d7bd5dc3e5835be01a81c9c37e587df8d
Moobi Kabelo <giftmoobi@gmail.com>
2023-06-29 00:53:50 +0200

    refactortored to server to https

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,20 @@
  */
 
 import dotenv from 'dotenv';
-import http from 'http';
+import https from 'https';
+import fs from 'fs';
 import ip from 'ip';
 import path from 'path';
 import os from 'os';
+import url from 'url';
 
 import { app } from './app';
 import { Database, activityLogger, errorLogger } from './libs/index';
 
 dotenv.config();
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /**
  * The port on which the server will listen.
@@ -25,7 +30,14 @@ const PORT: number = parseInt(process.env.PORT as string, 10) || 5500;
 /**
  * Create an HTTP server instance using the Express app.
  */
-const server: http.Server = http.createServer(app);
+const server: https.Server = https.createServer(
+ {
+  key: fs.readFileSync(path.join(__dirname, './config/oxide-commerce.key')),
+  cert: fs.readFileSync(path.join(__dirname, './config/oxide-commerce.crt')),
+ },
+ app
+);
+//const server: http.Server = http.createServer(app);
 
 /**
  * Connect to the database and start the server.
@@ -38,6 +50,7 @@ Database.mongoConnect()
      name: os.hostname(),
      ip: ip.address(),
      port: PORT,
+     protocol: 'https',
      platform: os.platform(),
      path: path.resolve('.'),
     },
